e6767e7169d387b08d34c7cf26a2d265a8492383
rachachaia <96731000+rachachaia@users.noreply.github.com>
2022-03-13 17:15:11 +0100

    Add files via upload

---
+++ exo2.js
@@ -0,0 +1,9 @@
+//first method
+var X=[]; 
+var num=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+var index = 9;
+for (var i=index; i < num.length-1;i++);
+{
+    num[i]=num[i+1];
+}
+console.log(num);
