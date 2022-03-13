2cfdea8db8a5755ac3d0fe73ffaac3a5c7fcce92
rachachaia <96731000+rachachaia@users.noreply.github.com>
2022-03-13 17:17:01 +0100

    Delete exo2.js

--- exo2.js
+++
@@ -1,9 +0,0 @@
-//Remove a specific Element from an array ( no predefined functions allowed).
-var X=[]; 
-var num=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-var index = 9;
-for (var i=index; i < num.length-1;i++);
-{
-    num[i]=num[i+1];
-}
-console.log(num);
