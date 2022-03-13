f520c5ba10abfe95b58b76783a80af37666d8337
rachachaia <96731000+rachachaia@users.noreply.github.com>
2022-03-13 17:18:27 +0100

    Update exo2.js

--- exo2.js
+++ exo2.js
@@ -2,7 +2,7 @@
 var X=[]; 
 var num=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 var index = 9;
-for (var i=index; i < num.length-1;i++);
+for (var i=index; i < num.length-1;i++)
 {
     num[i]=num[i+1];
 }
